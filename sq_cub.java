a1cb318b93186a39fd6d17b2c0403225de46a514
Guru charan <88940490+guru2k3@users.noreply.github.com>
2022-09-14 09:37:39 -0400

    Create sq_cub.java
    Test cases:
    1. 12
    output:
    Square Number: 144.0
    Cube Number: 1728.0
    2. 0 
    output:
    Square Number: 0
    Cube Number: 0
    3. -0.5
    Output:
    Square Number: 0.25
    Cube Number: -0.125
    4. 14.25
    output:
    Square Number: 203.0625
    Cube Number: 2893.6406
    5. -296
    output:
    Square Number: 87616.0
    Cube Number: -2.5934336E7

---
+++ sq_cub.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    
+
+	public static void main(String[] args) 
+	{
+		float m;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number: ");
+		m=sc.nextFloat();
+		System.out.println("Square Number: "+(m*m)+"\n");
+		System.out.println("Cube Number: "+(m*m*m));
+	}
+}
